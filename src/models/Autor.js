0df62317a969131b614c3b3e5066627775bf9a7b
Alan <analisedesistemas20@gmail.com>
2024-02-20 08:57:35 -0300

    projeto inicial

---
+++ src/models/Autor.js
@@ -0,0 +1,24 @@
+// import mongoose from "mongoose";
+
+// const autorSchema = new mongoose.Schema({
+//     id: { type: mongoose.Schema.Types>ObjectId },
+//     nome: { type: String, required: true },
+//     nacionalidade: { type: String }
+// }, { versionKey: false });
+
+// const autor = mongoose.model("autores", autorSchema);
+
+// export { autor, autorSchema };
+
+
+import mongoose from "mongoose";
+
+const autorSchema = new mongoose. Schema ( {
+    id: { type: mongoose. Schema. Types. ObjectId },
+    nome: { type: String, required: true },
+    nacionalidade: { type: String }
+}, { versionkey: false });
+
+const autor = mongoose.model("autores", autorSchema) ;
+
+export { autor, autorSchema };
